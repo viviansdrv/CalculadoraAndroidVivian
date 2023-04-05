fd780529c3a4c2653e47f827ee7460ced08a5149
viviansdrv <83466012+viviansdrv@users.noreply.github.com>
2023-04-04 22:48:03 -0300

    Initial commit

---
+++ app/src/main/java/com/example/calculadoravivian/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.calculadoravivian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNro1;
+    private EditText txtNro2;
+    private TextView tvResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtNro1 = findViewById(R.id.txtNro1);
+        txtNro2 = findViewById(R.id.txtNro2);
+        tvResultado = findViewById(R.id.tvResultado);
+    }
+
+    public void somar(View view){
+        int valor1 = Integer.parseInt(txtNro1.getText().toString());
+        int valor2 = Integer.parseInt(txtNro2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 + valor2));
+    }
+
+    public void subtrair(View view){
+        int valor1 = Integer.parseInt(txtNro1.getText().toString());
+        int valor2 = Integer.parseInt(txtNro2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 - valor2));
+    }
+
+    public void multiplicar(View view){
+        int valor1 = Integer.parseInt(txtNro1.getText().toString());
+        int valor2 = Integer.parseInt(txtNro2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 * valor2));
+    }
+
+    public void dividir(View view){
+        int valor1 = Integer.parseInt(txtNro1.getText().toString());
+        int valor2 = Integer.parseInt(txtNro2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 / valor2));
+    }
+}
